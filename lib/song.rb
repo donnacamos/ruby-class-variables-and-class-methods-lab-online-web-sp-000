b6b910760a0ba8a7b3a1f8ac65ff5bc4e8537e2f
Donna Amos <donnacamos.developer@gmail.com>
2019-03-14 02:47:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,16 @@ class Song
    genre_count 
  end 
  
- def self.artist
+ def self.artist_count 
+   artist_count = {}
+   @@artists.each do |artist|
+     if artist_count[artist]
+       artist_count += 1 
+     else 
+       artist_count[artist] = 1 
+     end 
+   artist_count 
+ end 
+ end 
   
 end 
